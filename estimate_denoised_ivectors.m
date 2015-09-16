517a65811cbc9e3daba8eae981fc04a5c1384310
Hamid <h.eghbalz@gmail.com>
2015-09-16 15:03:50 +0200

    Update estimate_denoised_ivectors.m

--- estimate_denoised_ivectors.m
+++ estimate_denoised_ivectors.m
@@ -1,4 +1,10 @@
 function denoised_test_ivec = estimate_denoised_ivectors(noisy_train_ivec,clean_train_ivec,noisy_test_ivec)
+% ivector denoising method based on ICASSP'15 paper: ADDITIVE NOISE COMPENSATION IN THE I-VECTOR SPACE FOR SPEAKER RECOGNITION
+% noisy_train_ivec: ivectors trained on noisy data
+% clean_train_ivec: ivectors trained on clean data
+% noisy_test_ivec: test noisy ivectors to be denoised
+% denoised_test_ivec: denoised test ivectors
+
 estimated_noise_train_ivec=noisy_train_ivec-clean_train_ivec;
 sigma_noise=cov(estimated_noise_train_ivec)^-1;
 sigma_clean=cov(clean_train_ivec)^-1;
